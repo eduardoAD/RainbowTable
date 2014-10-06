a01f47ace919f308fa27b3e77cab500e8d40762b
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-06 12:41:15 -0500

    Initial Commit

---
+++ RainbowTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RainbowTable
+//
+//  Created by Eduardo Alvarado Díaz on 10/6/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RainbowTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RainbowTable
+//
+//  Created by Eduardo Alvarado Díaz on 10/6/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
