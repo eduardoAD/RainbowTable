b58b766c3696086c0f62fb793e030928bf6d092a
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-07 14:15:25 -0500

    hackwich

--- RainbowTable/ViewController.m
+++ RainbowTable/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property NSArray *colorsArray;
 
 @end
 
@@ -16,12 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.colorsArray = [NSArray arrayWithObjects:[UIColor redColor],
+                        [UIColor orangeColor],
+                        [UIColor yellowColor],
+                        [UIColor greenColor],
+                        [UIColor blueColor],
+                        [UIColor colorWithRed:(75.0/255.0) green:(0/255.00) blue:(130.0/255.0) alpha:(1)],
+                        [UIColor purpleColor],
+                        nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.colorsArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"colorCellId" forIndexPath:indexPath];
+
+    //Its gonna dequeue, it will reuse the cells, so it doesnt have to create thousands of cells
+    //Depending of the object is next, it put the cell.
+    //cell.textLabel.text = [NSString stringWithFormat: @"row %i",
+
+    //cell.textLabel.text = [NSString stringWithFormat: @"row %li", (long)indexPath.row];
+    cell.backgroundColor = [self.colorsArray objectAtIndex:indexPath.row];
+    
+    return cell;
 }
 
 @end
